5634d7b0a4f49f2b263500f6b9ada07605f5fa25
Parle648 <morozovalex842@gmail.com>
2024-03-03 14:37:34 +0200

    Fix: fix header order Link

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../features/Header/Header';
 import CatalogPage from '../pages/CatalogPage/CatalogPage';
 import OrderPage from '../pages/OrderPage/OrderPage';
 import OrdersPage from '../pages/OrdersPage/OrdersPage';
 import CouponesPage from '../pages/CouponesPage/CouponesPage';
-
-// const router = createBrowserRouter([{
-//   path: '/',
-//   element: <CatalogPage />
-// }, {
-//   path: '/order-page',
-//   element: <OrderPage />
-// }, {
-//   path: '/orders',
-//   element: <OrdersPage />
-// }, {
-//   path: '/coupones',
-//   element: <CouponesPage />
-// }, ]);
 
 function App() {
   return (
